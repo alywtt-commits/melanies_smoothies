73cd770e6dff8c12a23603fca8023beb5f1d37cd
alywtt-commits <alywtt@gmail.com>
2026-07-23 10:06:49 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,7 +39,6 @@ if ingredients_list:
 
 import requests  
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon" )  
-st.text(smoothiefroot_response)
+st.text(smoothiefroot_response.json())
 
-# smoothiefroot_response = requests.get("(https://my.smoothiefroot.com/api/fruit/watermelon)")  
-#st.text(smoothiefroot_response)
+ 
